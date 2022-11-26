2ad20b990c8abadfaca0540026c537c8dd33e0d8
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-26 01:29:51 +0000

    feat: show modal on first load

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const consultarTituloDeLaPagina = () => {
         actualizarTitulo("Es la primera vez que entraste");
         localStorage.setItem("tituloPagina", "Ya entraste antes");
         localStorage.setItem("contadorDeIngresos", "1");
+        crearModal();
     } else {
         localStorage.setItem("contadorDeIngresos", `${contadorDeIngresos + 1}`);
         actualizarTitulo(titulo);
@@ -23,8 +24,21 @@ const crearModal = () => {
     const app = document.querySelector("#app");
     const body = app.parentNode;
     const modalContainer = document.createElement("div");
+    modalContainer.setAttribute("class", "modal");
+    const modalDialog = document.createElement("div");
+    modalDialog.setAttribute("class", "modal-dialog");
     body.append(modalContainer);
-
+    modalContainer.append(modalDialog);
+    const modalContent = document.createElement("div");
+    modalContent.setAttribute("class", "modal-content");
+    modalDialog.append(modalContent);
+    const modalBody = document.createElement("div")
+    modalBody.setAttribute("class", "modal-body");
+    modalDialog.append(modalBody);
+    const parrafo = document.createElement("p")
+    parrafo.innerText = "ESTA FUNCIONANDO EL MODAL";
+    modalBody.append(parrafo);
+    
+    modalContainer.style.display = "block";
 }
 consultarTituloDeLaPagina();
-crearModal();
