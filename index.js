3585f64073b45394c3379e865ea401033e5d1a1a
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-26 01:12:28 +0000

    feat: add page load counter

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const actualizarTitulo = () => {
+const actualizarTitulo = (titulo) => {
     const app = document.querySelector("#app");
     const html = app.parentNode.parentElement
     const head = html.children[0]
     const title = head.children[4];
-    title.innerHTML = "Jugando con localStorage";
+    title.innerHTML = titulo;
 }
+
+const consultarTituloDeLaPagina = () => {
+    const titulo = localStorage.getItem("tituloPagina");
+    const contadorDeIngresos = +localStorage.getItem("contadorDeIngresos");
+    if(!contadorDeIngresos) {
+        actualizarTitulo("Es la primera vez que entraste");
+        localStorage.setItem("tituloPagina", "Ya entraste antes");
+        localStorage.setItem("contadorDeIngresos", "1");
+    } else {
+        localStorage.setItem("contadorDeIngresos", `${contadorDeIngresos + 1}`);
+        actualizarTitulo(titulo);
+    }
+}
+
+consultarTituloDeLaPagina();
